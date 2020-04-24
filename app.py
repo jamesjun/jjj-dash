c30113915af6275d8e34f1409604f8fb9b4b65d8
James Jun <jamesjun@gmail.com>
2020-04-24 17:38:38 -0400

    markdown, scatter, input and output

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
-import dash
+import os, dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 server = app.server
@@ -12,37 +12,69 @@ df = pd.read_csv(
     'https://gist.githubusercontent.com/chriddyp/' +
     '5d1ea79569ed194d432e56108a04d188/raw/' +
     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
+    'gdp-life-exp-2007.csv'
+)
+
+markdown_text = '''
+### Data selection scatter plot example
+[Source](https://www.datacamp.com/community/tutorials/learn-build-dash-python)
+'''
+
+app.layout = html.Div([
+    dcc.Markdown(children=markdown_text)
+])
+
+fig = go.Figure() 
+for i in df.continent.unique():
+    fig.add_trace(
+        go.Scatter(
+            x=df[df['continent'] == i]['gdp per capita'],
+            y=df[df['continent'] == i]['life expectancy'],
+            text=df[df['continent'] == i]['country'],
+            mode='markers',
+            opacity=0.8,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        )
+    )
+
+fig.layout = go.Layout(
+        xaxis=dict(type='log', title='GDP Per Capita'),
+        yaxis=dict(title='Life Expectancy'),
+        margin=dict(l=40, b=40, t=10, r=10),
+        legend=dict(x=0, y=1),
+        hovermode='closest'
+    )
 
 
 app.layout = html.Div([
+    dcc.Markdown(children=markdown_text),
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[{'label': i, 'value': i} for i in df.continent.unique()],
+        value=[df.continent.unique()[0]],
+        multi=False
+    ),
     dcc.Graph(
         id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.8,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
+        figure=fig
+    ),
+    html.Div(id='my-div')
 ])
 
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-dropdown', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
+
+
 if __name__ == '__main__':
+    #fig.write_image("fig1.svg")
     app.run_server(debug=True)
+    
