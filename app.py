b3db5562b326da5f2f01cac380937dbe1f24219b
James Jun <jamesjun@gmail.com>
2020-04-24 19:39:55 -0400

    two bar graphs added

--- app.py
+++ app.py
@@ -24,7 +24,18 @@ app.layout = html.Div([
     dcc.Markdown(children=markdown_text)
 ])
 
-def make_fig(list_continent):
+def make_fig_bar(list_continent, str_type):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=list_continent,
+        y=[df[df['continent']==i][str_type].mean() for i in list_continent],
+    ))
+    fig.update_layout(
+        yaxis=dict(title='Mean '+str_type)
+    )
+    return fig
+
+def make_fig_scatter(list_continent):
     fig = go.Figure()
     #for i in df.continent.unique():
     for i in list_continent:
@@ -43,16 +54,16 @@ def make_fig(list_continent):
             )
         )
 
-    fig.layout = go.Layout(
-            xaxis=dict(type='log', title='GDP Per Capita'),
-            yaxis=dict(title='Life Expectancy'),
-            margin=dict(l=40, b=40, t=10, r=10),
-            legend=dict(x=0, y=1),
-            hovermode='closest'
-        )
+    fig.update_layout(
+        xaxis=dict(type='log', title='GDP Per Capita'),
+        yaxis=dict(title='Life Expectancy'),
+        margin=dict(l=40, b=40, t=10, r=10),
+        legend=dict(x=0, y=1),
+        hovermode='closest'
+    )
     return fig
 
-list_continent = [df.continent.unique()[0]]
+list_continent = df.continent.unique()
 #print(list_continent)
 
 app.layout = html.Div([
@@ -65,9 +76,25 @@ app.layout = html.Div([
         multi=True
     ),
     dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=make_fig(list_continent)
+        id='fig_scatter',
+        figure=make_fig_scatter(list_continent)
     ),
+    html.Table([
+        html.Tr([
+            html.Td(
+                dcc.Graph(
+                    id='fig_bar_gdp',
+                    figure=make_fig_bar(list_continent, 'gdp per capita')
+                ),
+            ),
+            html.Td(
+                dcc.Graph(
+                    id='fig_bar_life',
+                    figure=make_fig_bar(list_continent, 'life expectancy')
+                ),
+            )
+        ])
+    ]),
     html.Div(id='my-div')
 ])
 
@@ -79,11 +106,25 @@ def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
 
 @app.callback(
-    Output(component_id='life-exp-vs-gdp', component_property='figure'),
+    Output(component_id='fig_scatter', component_property='figure'),
     [Input(component_id='my-dropdown', component_property='value')]
 )
-def update_fig(input_value):
-    return make_fig(input_value)
+def update_fig_scatter(input_value):
+    return make_fig_scatter(input_value)
+
+@app.callback(
+    Output(component_id='fig_bar_gdp', component_property='figure'),
+    [Input(component_id='my-dropdown', component_property='value')]
+)
+def update_fig_bar_gdp(input_value):
+    return make_fig_bar(input_value, 'gdp per capita')
+
+@app.callback(
+    Output(component_id='fig_bar_life', component_property='figure'),
+    [Input(component_id='my-dropdown', component_property='value')]
+)
+def update_fig_bar_life(input_value):
+    return make_fig_bar(input_value, 'life expectancy')
 
 
 if __name__ == '__main__':
